85e1583b9d30f7ab834cab0efc1a5802fd113abe
Noemi <narcisaquintanilla25@gmail.com>
2024-12-26 20:02:44 -0500

    cambie la interfaz de las categorias

--- lib/vistaCategorias.dart
+++ lib/vistaCategorias.dart
@@ -7,10 +7,10 @@ class VistaCategorias extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Categorias Medicamentos"),
+        title: const Text("Categorías Medicamentos"),
         centerTitle: true,
         titleTextStyle: const TextStyle(
-          color: Color.fromARGB(255, 255, 255, 255), // Color del texto
+          color: Color(0xFFFFFFFF), // Color del texto
           fontSize: 20, // Tamaño de la fuente
           fontWeight: FontWeight.bold, // Grosor de la fuente
           fontFamily: 'Roboto', // Familia de la fuente
@@ -18,27 +18,48 @@ class VistaCategorias extends StatelessWidget {
         backgroundColor: const Color(0xFF254754),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.count(
-          crossAxisCount: 2, // Número de columnas
-          crossAxisSpacing: 10, // Espacio entre columnas
-          mainAxisSpacing: 10, // Espacio entre filas
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
           children: [
-            _buildCard(context, 'assets/images/imagen1.png', 'Antibióticos',
-                '/rutaAntibioticos'),
-            _buildCard(context, 'assets/images/imagen2.png', 'Analgésicos',
-                '/rutaAnalgesicos'),
-            _buildCard(context, 'assets/images/imagen3.png',
-                'Antiinflamatorios', '/rutaAntiinflamatorios'),
-            _buildCard(context, 'assets/images/imagen4.png', 'Antidepresivos',
+            _buildListCard(context, 'assets/images/imagen1.png', 'Antibióticos',
+                'Administra antibióticos', '/rutaAntibioticos'),
+            _buildListCard(context, 'assets/images/imagen2.png', 'Analgésicos',
+                'Administra analgésicos', '/rutaAnalgesicos'),
+            _buildListCard(
+                context,
+                'assets/images/imagen3.png',
+                'Antiinflamatorios',
+                'Administra antiinflamatorios',
+                '/rutaAntiinflamatorios'),
+            _buildListCard(
+                context,
+                'assets/images/imagen4.png',
+                'Antidepresivos',
+                'Administra antidepresivos',
                 '/rutaAntidepresivos'),
-            _buildCard(context, 'assets/images/imagen5.png', 'Antialérgicos',
+            _buildListCard(
+                context,
+                'assets/images/imagen5.png',
+                'Antialérgicos',
+                'Administra antialérgicos',
                 '/rutaAntialergicos'),
-            _buildCard(context, 'assets/images/imagen6.png', 'Antipiréticos',
+            _buildListCard(
+                context,
+                'assets/images/imagen6.png',
+                'Antipiréticos',
+                'Administra antipiréticos',
                 '/rutaAntipireticos'),
-            _buildCard(context, 'assets/images/imagen1.png', 'Antialérgicos',
-                '/rutaAntialergicos'),
-            _buildCard(context, 'assets/images/imagen2.png', 'Antipiréticos',
+            _buildListCard(
+                context,
+                'assets/images/imagen6.png',
+                'Antipiréticos',
+                'Administra antipiréticos',
+                '/rutaAntipireticos'),
+            _buildListCard(
+                context,
+                'assets/images/imagen6.png',
+                'Antipiréticos',
+                'Administra antipiréticos',
                 '/rutaAntipireticos'),
           ],
         ),
@@ -46,47 +67,39 @@ class VistaCategorias extends StatelessWidget {
     );
   }
 
-  // Función para crear una tarjeta que navega a una ruta específica
-  Widget _buildCard(
-      BuildContext context, String imagePath, String title, String routeName) {
+  // Función para crear una tarjeta en formato de lista
+  Widget _buildListCard(BuildContext context, String imagePath, String title,
+      String subtitle, String routeName) {
     return Card(
       elevation: 4, // Sombra de la tarjeta
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10), // Bordes redondeados
       ),
-      color: const Color(0xFF49b6c7), // Color de fondo de la tarjeta
-      child: InkWell(
+      child: ListTile(
         onTap: () {
           Navigator.pushNamed(context, routeName); // Navegación a otra ruta
         },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-                child: Image.asset(
-                  imagePath,
-                  fit: BoxFit.cover, // Ajusta la imagen al espacio
-                  width: double.infinity,
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-          ],
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+            width: 48,
+            height: 48,
+          ),
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(subtitle),
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.grey,
+          size: 16,
         ),
       ),
     );
